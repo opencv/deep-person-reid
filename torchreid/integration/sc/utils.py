3152625886e3caa7fdb5ef7f1107ac6d622002a2
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2022-03-09 15:19:26 +0300

    Silent pylint

--- torchreid/integration/sc/utils.py
+++ torchreid/integration/sc/utils.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
+# pylint: disable=too-many-nested-blocks
+
 from contextlib import contextmanager
 from enum import Enum, auto
 import importlib
